ccea27ade2b1b955e5573a13f2214b30d7d7a7f3
Rupesh Bobde <138480752+RupeshBobde69@users.noreply.github.com>
2025-07-13 16:14:11 +0530

    Add implementations for multiplication table, rectangle, square, and triangle patterns

---
+++ PATTERN/Rectangle.java
@@ -0,0 +1,12 @@
+package PATTERN;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        for(int i = 1 ;i <= 4 ; i++){
+            for(int j = 1 ; j <=20 ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
--- TABLE.java
+++ TABLE.java
@@ -1,3 +1,59 @@
+import java.util.Scanner;
+
 public class TABLE {
-    
+    public static void multi(int num) {
+        if (num >= 1 && num <= 100) {
+            System.out.println("-------------------");
+            for (int i = 1; i <= 10; i++) {
+                if (num < 10 && i < 10 && num * i < 10) {
+                    System.out.println("| " + num + "   X " + i + "  | " + num * i + "    |");
+                    System.out.println("-------------------");
+                } else if (num < 10 && i < 10 && num * i < 100) {
+                    System.out.println("| " + num + "   X " + i + "  | " + num * i + "   |");
+                    System.out.println("-------------------");
+                } else if (num < 10 && i == 10 && num * i < 100) {
+                    System.out.println("| " + num + "   X " + i + " | " + num * i + "   |");
+                    System.out.println("-------------------");
+                } else if (num == 10 && i < 10 && num * i < 100) {
+                    System.out.println("| " + num + "  X " + i + "  | " + num * i + "   |");
+                    System.out.println("-------------------");
+                } else if (num == 10 && i == 10 && num * i == 100) {
+                    System.out.println("| " + num + "  X " + i + " | " + num * i + "  |");
+                    System.out.println("-------------------");
+                } else if (num < 100 && i < 10 && num * i < 100) {
+                    System.out.println("| " + num + "  X " + i + "  | " + num * i + "   |");
+                    System.out.println("-------------------");
+                } else if (num < 100 && i < 10 && num * i < 1000) {
+                    System.out.println("| " + num + "  X " + i + "  | " + num * i + "  |");
+                    System.out.println("-------------------");
+                } else if (num < 100 && i == 10 && num * i < 1000) {
+                    System.out.println("| " + num + "  X " + i + " | " + num * i + "  |");
+                    System.out.println("-------------------");
+                } else if (num == 100 && i < 10 && num * i < 1000) {
+                    System.out.println("| " + num + " X " + i + "  | " + num * i + "  |");
+                    System.out.println("-------------------");
+                } else if (num == 100 && i == 10 && num * i == 1000) {
+                    System.out.println("| " + num + " X " + i + " | " + num * i + " |");
+                    System.out.println("-------------------");
+                }
+            }
+        } else {
+            System.out.println("not a vailed number.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String ans;
+        do {
+            System.out.println("enter the number between 1 to 100:");
+            int num = scanner.nextInt();
+            scanner.nextLine();
+            multi(num);
+            System.out.println("do you want to keep going:(yes/no)");
+            ans = scanner.nextLine().trim().toLowerCase();
+        } while (ans.equals("yes"));
+        System.out.println("system is closed....");
+        scanner.close();
+    }
 }
