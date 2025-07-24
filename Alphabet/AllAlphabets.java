0d30c8abd377c6906e2ccb3204e9f8cb0001b44c
Rupesh Bobde <138480752+RupeshBobde69@users.noreply.github.com>
2025-07-24 18:32:29 +0530

    Add implementations for letters C to Z in AllAlphabets class and update Main class to support new patterns

--- Alphabet/AllAlphabets.java
+++ Alphabet/AllAlphabets.java
@@ -20,28 +20,343 @@ public class AllAlphabets {
             System.out.println();
         }
     }
-    public void B(int size) {
-    for (int i = 1; i <= size; i++) {
-        for (int j = 1; j <= size; j++) {
-            // Print * for first column (vertical bar)
-            if (j == 1) {
-                System.out.print("*");
-            }
-            // Print * for top, middle, bottom horizontal lines
-            else if ((i == 1 || i == size || i == size / 2 + 1) && j < size) {
-                System.out.print("*");
-            }
-            // Print * for last column of top and bottom half
-            else if ((i < size / 2 + 1 && j == size) || (i > size / 2 + 1 && j == size)) {
-                System.out.print("*");
-            }
-            // Else print space
-            else {
-                System.out.print(" ");
-            }
-        }
-        System.out.println();
-    }
-}
 
+    public void B(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                // Print * for first column (vertical bar)
+                if (j == 1) {
+                    System.out.print("*");
+                }
+                // Print * for top, middle, bottom horizontal lines
+                else if ((i == 1 || i == size || i == size / 2 + 1) && j < size) {
+                    System.out.print("*");
+                }
+                // Print * for last column of top and bottom half
+                else if ((i < size / 2 + 1 && j == size) || (i > size / 2 + 1 && j == size)) {
+                    System.out.print("*");
+                }
+                // Else print space
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void C(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if ((i == 1 && j <= size) || (i == size && j <= size)) {
+                    System.out.print("*");
+                } else if (j == 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void D(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if ((i == 1 && j <= size - 1) || (i == size && j <= size - 1)) {
+                    System.out.print("*");
+                } else if ((j == 1) || ((i >= 2 && i <= size - 1) && j == size)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public void E(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (j == 1 || i == 1 || i == size || i == size/2 + 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void F(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (j == 1 || i == 1 || i == size/2 + 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void G(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i == 1 || i == size || j == 1 || (i >= size/2 && j == size) || (i == size/2 && j >= size/2)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void H(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (j == 1 || j == size || i == size/2 + 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void I(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i == 1 || i == size || j == size/2 + 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void J(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i == 1 || (j == size/2 + 1 && i != size) || (i == size && j <= size/2 + 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void K(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (j == 1 || j == size - i + 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void L(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (j == 1 || i == size) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void M(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (j == 1 || j == size || (i == j && i <= size/2 + 1) || (i + j == size + 1 && i <= size/2 + 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void N(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (j == 1 || j == size || i == j) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void O(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i == 1 || i == size || j == 1 || j == size) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void P(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (j == 1 || (i == 1 && j < size) || (i == size/2 + 1 && j < size) || (j == size && i > 1 && i < size/2 + 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void Q(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i == 1 || i == size - 1 || j == 1 || j == size - 1 || (i == j && i > size/2)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void R(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (j == 1 || (i == 1 && j < size) || (i == size/2 + 1 && j < size) || (j == size && i > 1 && i < size/2 + 1) || (i - j == size/2)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void S(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i == 1 || i == size/2 + 1 || i == size || (j == 1 && i < size/2 + 1 && i != 1) || (j == size && i > size/2 + 1 && i != size)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void T(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i == 1 || j == size/2 + 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void U(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if ((j == 1 && i != size) || (j == size && i != size) || (i == size && j > 1 && j < size)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void V(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size * 2 - 1; j++) {
+                if (j == i || j == size * 2 - i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void W(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size * 2 - 1; j++) {
+                if (j == 1 || j == size * 2 - 1 || (i == j && j >= size) || (i + j == size * 2 && j <= size)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void X(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i == j || i + j == size + 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void Y(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if ((i <= size/2 && (j == i || j == size - i + 1)) || (i > size/2 && j == size/2 + 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void Z(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i == 1 || i == size || j == size - i + 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
--- Alphabet/Main.java
+++ Alphabet/Main.java
@@ -15,6 +15,7 @@ public class Main {
             }
             System.out.println("enter alphabet size:");
             int size = sc.nextInt();
+            sc.nextLine();
             switch (alphabet) {
                 case "A":
                     Alphabet.A(size);
@@ -22,11 +23,84 @@ public class Main {
                 case "B":
                     Alphabet.B(size);
                     break;
+                case "C":
+                    Alphabet.C(size);
+                    break;
+                case "D":
+                    Alphabet.D(size);
+                    break;
+                case "E":
+                    Alphabet.E(size);
+                    break;
+                case "F":
+                    Alphabet.F(size);
+                    break;
+                case "G":
+                    Alphabet.G(size);
+                    break;
+                case "H":
+                    Alphabet.H(size);
+                    break;
+                case "I":
+                    Alphabet.I(size);
+                    break;
+                case "J":
+                    Alphabet.J(size);
+                    break;
+                case "K":
+                    Alphabet.K(size);
+                    break;
+                case "L":
+                    Alphabet.L(size);
+                    break;
+                case "M":
+                    Alphabet.M(size);
+                    break;
+                case "N":
+                    Alphabet.N(size);
+                    break;
+                case "O":
+                    Alphabet.O(size);
+                    break;
+                case "P":
+                    Alphabet.P(size);
+                    break;
+                case "Q":
+                    Alphabet.Q(size);
+                    break;
+                case "R":
+                    Alphabet.R(size);
+                    break;
+                case "S":
+                    Alphabet.S(size);
+                    break;
+                case "T":
+                    Alphabet.T(size);
+                    break;
+                case "U":
+                    Alphabet.U(size);
+                    break;
+                case "V":
+                    Alphabet.V(size);
+                    break;
+                case "W":
+                    Alphabet.W(size);
+                    break;
+                case "X":
+                    Alphabet.X(size);
+                    break;
+                case "Y":
+                    Alphabet.Y(size);
+                    break;
+                case "Z":
+                    Alphabet.Z(size);
+                    break;
                 default:
+                    System.out.println("Pattern not available for this letter yet.");
                     break;
             }
             System.out.println("do u want to continue making alphabets(yes/no):");
-            ans = sc.nextLine().toLowerCase().trim();
+            ans = sc.nextLine().toLowerCase();
         } while (ans.equals("yes"));
         sc.close();
     }
